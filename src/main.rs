db1f1e6f310b32f2f4f92258ddc44598b91b6795
k-yomo <kanji.yy@gmail.com>
2020-10-14 21:17:21 +0900

    Configure settings to develop operating system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // do not link Rust std lib
+#![no_main] // disable all Rust-level entry points
+use core::panic::PanicInfo;
+
+// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point
+    loop {}
 }
