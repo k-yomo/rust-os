c2c2fbf2bc1c77f330b972634c21fb4cd3901b60
k-yomo <kanji.yy@gmail.com>
2020-10-14 22:49:28 +0900

    Encapsulate print logic in the vga_buffer module

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@
 #![no_main] // disable all Rust-level entry points
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -12,14 +15,8 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // color byte represents light cyan
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("panicked!");
 
     loop {}
 }
